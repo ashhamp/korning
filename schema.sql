aed97d0582c4e37a84ba6851473bd559d922343e
Ashley <ashhamp27@gmail.com>
2016-03-08 15:48:57 -0500

    added unique columns

--- schema.sql
+++ schema.sql
@@ -1,7 +1,6 @@
 
 
 DROP TABLE IF EXISTS transactions;
-DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS products;
@@ -11,18 +10,18 @@ DROP TABLE IF EXISTS products;
 CREATE TABLE employees (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100),
-  email VARCHAR(100)
+  email VARCHAR(100) unique
 );
 
 CREATE TABLE customers (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100),
-  account_number VARCHAR(100)
+  account_number VARCHAR(100) unique
 );
 
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(100)
+  name VARCHAR(100) unique
 );
 
 CREATE TABLE transactions (
